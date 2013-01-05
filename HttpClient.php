1c21cefb8939e2dfa087c32c4ac0da8860dec482
Michael Bemmerl <mail@mx-server.de>
2013-01-06 00:01:25 +0100

    Implemented a simple HTTP client.

---
+++ HttpClient.php
@@ -0,0 +1,44 @@
+<?php
+
+class HttpClient
+{
+	public $status;
+
+	public $response;
+
+	public function get($url)
+	{
+		$this->response = @file_get_contents($url);
+
+		if (isset($http_response_header))
+		{
+			$this->parseResponseHeader($http_response_header);
+			return TRUE;
+		}
+		else
+		{
+			$this->response = NULL;
+			return FALSE;
+		}
+	}
+
+	private function parseResponseHeader($header)
+	{
+		// HTTP/1.1 200 OK
+		$pattern = '#HTTP/\d\.\d (\d{3})#';
+
+		// Clear fields
+		$this->status = NULL;
+
+		foreach($header as $entry)
+		{
+			$match = preg_match($pattern, $entry, $matches);
+
+			if ($match == 1)
+			{
+				$this->status = (int)$matches[1];
+				break;
+			}
+		}
+	}
+}
