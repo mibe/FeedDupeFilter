e1ce86e62feb354b9c4d0f9e1b9e2ec49cb61136
Michael Bemmerl <mail@mx-server.de>
2013-01-06 04:59:05 +0100

    Using the new FeedItem & FeedManipulator in Core.

--- Core.php
+++ Core.php
@@ -2,6 +2,12 @@
 
 require('ArchiveBase.php');
 require('FileArchive.php');
+
+require('FeedItemBase.php');
+require('Rss2FeedItem.php');
+require('FeedManipulatorBase.php');
+require('Rss2FeedManipulator.php');
+
 require('HttpClient.php');
 
 class Core
@@ -9,6 +15,7 @@ class Core
 	private $feedUrl;
 	private $archive;
 	private $http;
+	private $feedManipulator;
 
 	function __construct($feedUrl)
 	{
@@ -16,14 +23,31 @@ class Core
 
 		$this->archive = new FileArchive($feedUrl);
 		$this->http = new HttpClient();
+
+		$this->fetchFeed();
+		$this->detectManipulator();
 	}
 
-	public function Filter()
+	private function detectManipulator()
 	{
+		$rss2 = new Rss2FeedManipulator($this->http->response);
+
+		if ($rss2->isSupported())
+			$this->feedManipulator = $rss2;
+	}
+
+	private function fetchFeed()
+	{
+		// Retrieve Feed
 		$result = $this->http->get($this->feedUrl);
 
 		if ($result === FALSE || $this->http->status != 200)
 			$this->handleHttpErrors();
+	}
+
+	public function Filter()
+	{
+		$this->feedManipulator->parseFeed();
 	}
 
 	private function handleHttpErrors()
