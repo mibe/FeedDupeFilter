4165384114c26bd17c4acc8ccd03fbab0512d9a4
Michael Bemmerl <mail@mx-server.de>
2013-01-06 04:20:54 +0100

    Added function for retrieving value of the next XML child element.

--- FeedItemBase.php
+++ FeedItemBase.php
@@ -10,4 +10,14 @@ abstract class FeedItemBase
 	public $id;
 
 	abstract public function parseXml($xml);
+
+	protected function getXmlChildValue($domElement, $name)
+	{
+		$list = $domElement->getElementsByTagName($name);
+
+		if ($list->length == 0)
+			return NULL;
+		else
+			return $list->item(0)->nodeValue;
+	}
 }
